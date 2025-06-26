45858f7232d227554b6a28af6ae5bd205f98c6f0
Fatima Nur <fatimanur424@example.com>
2025-06-26 18:03:45 +0500

    Footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes, useLocation } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import {Toaster} from 'react-hot-toast'
+import Footer from './components/Footer'
 const App = () => {
   const isSellerPath = useLocation().pathname.includes('seller')
   return (
@@ -13,6 +14,7 @@ const App = () => {
           <Route path='/' element={<Home/>}/> 
         </Routes>
       </div>
+      {!isSellerPath && <Footer/>}
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,6 +13,7 @@ const Home = () => {
 <BottomBanner/>
 <NewsLetter/>
 
+
     </div>
   )
 }
