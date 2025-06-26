8bccd3fb03fc6957a5d81dbb8716025a4540a34c
Fatima Nur <fatimanur424@example.com>
2025-06-26 22:42:45 +0500

    get count and amount functionality

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -54,10 +54,31 @@ export const AppContextProvider = ({children}) => {
         toast.success("Removed from cart")
         setCartItems(cartData)
     }
+
+    // Get Cart item count
+    const getCartCount = ()=>{
+        let totalCount = 0;
+        for(const item in cartItems){
+            totalCount += cartItems[item]
+        }
+        return totalCount;
+    }
+ // get cart total amount
+ const getCartAmount = ()=> {
+    let totalAmount = 0;
+    for( const items in cartItems){
+        let itemInfo = products.find((product) =>product>_id === items);
+        if(cartItems[items] > 0 ){
+            totalAmount += itemInfo.offerPrice * cartItems[items]
+        }
+        return Math.floor(totalAmount * 100 ) / 100;
+    }
+ }
+
     useEffect(() => {
         fetchProducts()
     }, [])
-    const value= { navigate, user, setUser,isSeller, setIsSeller, showUserLogin, setShowUserLogin, products, setProducts, currency, addToCart, updateCartItem, removeFromCart, cartItems, searchQuery, setSearchQuery }
+    const value= { navigate, user, setUser,isSeller, setIsSeller, showUserLogin, setShowUserLogin, products, setProducts, currency, addToCart, updateCartItem, removeFromCart, cartItems, searchQuery, setSearchQuery, getCartAmount, getCartCount }
       return <Appcontext.Provider value={value}>{children}</Appcontext.Provider>;
 }
 
