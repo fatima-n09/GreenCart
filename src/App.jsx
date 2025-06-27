b7cda3241d97a0ebbf722cde2f73ac8f0ffe69ad
Fatima Nur <fatimanur424@example.com>
2025-06-27 22:57:42 +0500

    Sellerlogin page

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,45 @@
-import { Route, Routes, useLocation } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Home from './pages/Home'
-import {Toaster} from 'react-hot-toast'
-import Footer from './components/Footer'
-import { useAppContext } from './context/AppContext'
-import Login from './components/Login'
-import AllProducts from './pages/AllProducts'
-import ProductCategory from './pages/ProductCategory'
-import ProductDetails from './pages/ProductDetails'
-import Cart from './pages/Cart'
-import AddAddress from './pages/AddAddress'
-import MyOrders from './pages/MyOrders'
+import { Route, Routes, useLocation } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import { Toaster } from "react-hot-toast";
+import Footer from "./components/Footer";
+import { useAppContext } from "./context/AppContext";
+import Login from "./components/Login";
+import AllProducts from "./pages/AllProducts";
+import ProductCategory from "./pages/ProductCategory";
+import ProductDetails from "./pages/ProductDetails";
+import Cart from "./pages/Cart";
+import AddAddress from "./pages/AddAddress";
+import MyOrders from "./pages/MyOrders";
+import SellerLogin from "./components/Seller/SellerLogin";
 const App = () => {
-  const isSellerPath = useLocation().pathname.includes('seller')
-  const{showUserLogin} = useAppContext();
+  const isSellerPath = useLocation().pathname.includes("seller");
+  const { showUserLogin, isSeller } = useAppContext();
   return (
     <div>
-      {isSellerPath ? null: <Navbar/>}
-      {showUserLogin ? <Login/> : null}
-      <Toaster/>
-      <div className={`${isSellerPath ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"}`}>
+      {isSellerPath ? null : <Navbar />}
+      {showUserLogin ? <Login /> : null}
+      <Toaster />
+      <div
+        className={`${isSellerPath ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"}`}
+      >
         <Routes>
-          <Route path='/' element={<Home/>}/> 
-          <Route path='/products' element={<AllProducts/>}/>
-          <Route path='/products/:category' element={<ProductCategory/>}/>
-          <Route path='/products/:category/:id' element={<ProductDetails/>}/>
-          <Route path='/cart' element={<Cart/>}/>
-          <Route path='/add-address' element={<AddAddress/>}/>
-          <Route path='/my-orders' element={<MyOrders/>}/>
-
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<AllProducts />} />
+          <Route path="/products/:category" element={<ProductCategory />} />
+          <Route path="/products/:category/:id" element={<ProductDetails />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/add-address" element={<AddAddress />} />
+          <Route path="/my-orders" element={<MyOrders />} />
+          <Route
+            path="/seller"
+            element={isSeller ? null : <SellerLogin />}
+          ></Route>
         </Routes>
       </div>
-      {!isSellerPath && <Footer/>}
+      {!isSellerPath && <Footer />}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
