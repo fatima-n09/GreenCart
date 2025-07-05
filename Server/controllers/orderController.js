c9237d6e2ca443b618617a9c97b59535fe79fc67
Fatima Nur <fatimanur424@example.com>
2025-07-05 23:05:58 +0500

    qqq

---
+++ Server/controllers/orderController.js
@@ -0,0 +1,68 @@
+
+import Order from "../models/order.js";
+import Product from "../models/Product.js";
+
+// place order COD  :/api/order/cod
+export const placeOrderCOD = async (req, res) => {
+  try {
+    const { userId, items, address } = req.body;
+    if (!address || items.length === 0) {
+      return res.json({ success: false, message: "Invalid Data" });
+    }
+
+    // Calculate amount using items
+    let amount = await items.reduce(async (acc, item) => {
+      const product = await Product.findById(item.product);
+      return (await acc) + productRouter.offerPrice * item.quantity;
+    }, 0);
+
+    // Add tax charge (2%)
+    amount += Math.floor(amount * 0.02);
+
+    await Order.create({
+      userId,
+      items,
+      amount,
+      address,
+      paymentType: "COD",
+    });
+
+    res.json({ success: true, message: "Order Placed Successfully" });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// get order by userid : /api/order/user
+
+export const getUserOrders = async (req, res) => {
+  try {
+    const { userId } = req.body;
+    const orders = await Order.find({
+      userId,
+      $or: [{ paymentType: "COD" }, { isPaid: true }],
+    })
+      .populate("items.product address")
+      .sort({ createdAt: -1 });
+
+    res.json({ success: true, orders });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// get all orders(for seller / admin) : /api/order/seller
+
+export const getAllOrders = async (req, res) => {
+  try {
+    const orders = await Order.find({
+      $or: [{ paymentType: "COD" }, { isPaid: true }],
+    })
+      .populate("items.product address")
+      .sort({ createdAt: -1 });
+
+    res.json({ success: true, orders });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
